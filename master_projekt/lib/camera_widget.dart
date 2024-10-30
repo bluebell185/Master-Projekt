77e033aef649d894845190527c7f86e2f5062e2a
kathrin.ulmer <64363092+bluebell185@users.noreply.github.com>
2024-10-30 09:50:02 +0100

    Code aufräumen

--- master_projekt/lib/camera_widget.dart
+++ master_projekt/lib/camera_widget.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'dart:math';
 
 import 'package:camera/camera.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
@@ -37,7 +36,7 @@ class _CameraWidgetState extends State<CameraWidget> {
 
     // Auswahl der Selfie-Kamera
     final CameraDescription selfieCam = chooseSelfieCamera();
-   
+
     // Kamera initialisieren - Hier Audio auf false, da sonst auch nach Mikrofonberechtigung gefragt wird
     controller = CameraController(selfieCam, ResolutionPreset.medium,
         enableAudio: false,
@@ -49,14 +48,6 @@ class _CameraWidgetState extends State<CameraWidget> {
       if (!mounted) {
         return;
       }
-      //  controller.startImageStream(_processCameraImage).then((value) {
-      //   if (widget.onCameraFeedReady != null) {
-      //     widget.onCameraFeedReady!();
-      //   }
-      //   if (widget.onCameraLensDirectionChanged != null) {
-      //     widget.onCameraLensDirectionChanged!(camera.lensDirection);
-      //   }
-      // });
       setState(() {});
 
       await controller.startImageStream((CameraImage image) async {
@@ -64,10 +55,7 @@ class _CameraWidgetState extends State<CameraWidget> {
         // CameraDescription description = controller.description;
         final visionImage =
             inputImageFromCameraImage(image, selfieCam, controller);
-        // InputImage visionImage = InputImage.fromBytes(
-        //     concatenatePlanes(image.planes),
-        //     buildMetaData(image,
-        //         rotationIntToImageRotation(description.sensorOrientation)));
+
         final List<Face> faces = await faceDetector.processImage(visionImage!);
         getFaceData(faces);
       });
@@ -86,12 +74,6 @@ class _CameraWidgetState extends State<CameraWidget> {
     });
   }
 
-  // void _processCameraImage(CameraImage image) {
-  //   final inputImage = inputImageFromCameraImage(image, camera, controller);
-  //   if (inputImage == null) return;
-  //   widget.onImage(inputImage);
-  // }
-
   CameraDescription chooseSelfieCamera() {
     for (final camera in camerasOfPhone) {
       if (camera.lensDirection == CameraLensDirection.front) {
@@ -104,6 +86,7 @@ class _CameraWidgetState extends State<CameraWidget> {
 
   @override
   void dispose() {
+    // TODO:
     // faceDetector.close();
     controller.dispose();
     super.dispose();
@@ -115,18 +98,12 @@ class _CameraWidgetState extends State<CameraWidget> {
       return Container();
     }
 
-    // Solution by https://stackoverflow.com/a/61487358
+    // Lösung für ein verzerrtes Kamerabild von: https://stackoverflow.com/a/61487358
     var camera = controller.value;
-    // fetch screen size
+    // Bildschirmgröße ermitteln
     final size = MediaQuery.of(context).size;
 
-    // calculate scale depending on screen and camera ratios
-    // this is actually size.aspectRatio / (1 / camera.aspectRatio)
-    // because camera preview size is received as landscape
-    // but we're calculating for portrait orientation
     var scale = size.aspectRatio * camera.aspectRatio;
-
-    // to prevent scaling down, invert the value
     if (scale < 1) scale = 1 / scale;
 
     return Transform.scale(
@@ -157,93 +134,53 @@ final _orientations = {
 // Code Großteils von https://github.com/flutter-ml/google_ml_kit_flutter/tree/master/packages/google_mlkit_commons#creating-an-inputimage
 InputImage? inputImageFromCameraImage(
     CameraImage image, CameraDescription camera, CameraController controller) {
-  // get image rotation
-  // it is used in android to convert the InputImage from Dart to Java
-  // `rotation` is not used in iOS to convert the InputImage from Dart to Obj-C
-  // in both platforms `rotation` and `camera.lensDirection` can be used to compensate `x` and `y` coordinates on a canvas
+  // Bildrotation
   final sensorOrientation = camera.sensorOrientation;
   InputImageRotation? rotation;
   if (Platform.isIOS) {
     rotation = InputImageRotationValue.fromRawValue(sensorOrientation);
   } else if (Platform.isAndroid) {
     var rotationCompensation =
-        _orientations[controller!.value.deviceOrientation];
+        _orientations[controller.value.deviceOrientation];
     if (rotationCompensation == null) return null;
-    if (camera.lensDirection == CameraLensDirection.front) {
-      // front-facing
-      rotationCompensation = (sensorOrientation + rotationCompensation) % 360;
-    } else {
-      // back-facing
-      rotationCompensation =
-          (sensorOrientation - rotationCompensation + 360) % 360;
-    }
+    rotationCompensation = (sensorOrientation + rotationCompensation) % 360;
     rotation = InputImageRotationValue.fromRawValue(rotationCompensation);
   }
   if (rotation == null) return null;
 
-  // get image format
+  // Bild Format
   final format = InputImageFormatValue.fromRawValue(image.format.raw);
-  // validate format depending on platform
-  // only supported formats:
-  // * nv21 for Android
-  // * bgra8888 for iOS
+  // Funktioniert nur mit
+  // * nv21 für Android
+  // * bgra8888 für iOS
   if (format == null ||
       (Platform.isAndroid && format != InputImageFormat.nv21) ||
       (Platform.isIOS && format != InputImageFormat.bgra8888)) return null;
 
-  // since format is constraint to nv21 or bgra8888, both only have one plane
+  // beide gültige Formate besitzen nur eine Dimension
   if (image.planes.length != 1) return null;
   final plane = image.planes.first;
 
-  // compose InputImage using bytes
+  // InputImage zusammenstellen
   return InputImage.fromBytes(
     bytes: plane.bytes,
     metadata: InputImageMetadata(
       size: Size(image.width.toDouble(), image.height.toDouble()),
-      rotation: rotation, // used only in Android
-      format: format, // used only in iOS
-      bytesPerRow: plane.bytesPerRow, // used only in iOS
+      rotation: rotation,
+      format: format,
+      bytesPerRow: plane.bytesPerRow,
     ),
   );
 }
 
-// InputImageMetadata buildMetaData(
-//   CameraImage image,
-//   InvalidType rotation,
-// ) {
-//   return GoogleVisionImageMetadata(
-//     rawFormat: image.format.raw,
-//     size: Size(image.width.toDouble(), image.height.toDouble()),
-//     rotation: rotation,
-//     planeData: image.planes.map(
-//       (Plane plane) {
-//         return GoogleVisionImagePlaneMetadata(
-//           bytesPerRow: plane.bytesPerRow,
-//           height: plane.height,
-//           width: plane.width,
-//         );
-//       },
-//     ).toList(),
-//   );
-// }
-
-// ImageRotation rotationIntToImageRotation(int rotation) {
-//   switch (rotation) {
-//     case 0:
-//       return ImageRotation.rotation0;
-//     case 90:
-//       return ImageRotation.rotation90;
-//     case 180:
-//       return ImageRotation.rotation180;
-//     default:
-//       assert(rotation == 270);
-//       return ImageRotation.rotation270;
-//   }
-// }
-
 void getFaceData(List<Face> faces) {
   for (Face face in faces) {
+    // TODO work with face data
     final Rect boundingBox = face.boundingBox;
+
+    final contours = face.contours;
+    FaceContour? faceContour = contours[FaceContourType.face];
+    faceContour?.points;
 
     final double? rotX =
         face.headEulerAngleX; // Head is tilted up and down rotX degrees
@@ -252,17 +189,10 @@ void getFaceData(List<Face> faces) {
     final double? rotZ =
         face.headEulerAngleZ; // Head is tilted sideways rotZ degrees
 
-    // If landmark detection was enabled with FaceDetectorOptions (mouth, ears,
-    // eyes, cheeks, and nose available):
     final FaceLandmark? leftEar = face.landmarks[FaceLandmarkType.leftEar];
     if (leftEar != null) {
       final Point<int> leftEarPos = leftEar.position;
     }
-
-    // If classification was enabled with FaceDetectorOptions:
-    // if (face.smilingProbability != null) {
-    //   final double? smileProb = face.smilingProbability;
-    // }
 
     // If face tracking was enabled with FaceDetectorOptions:
     if (face.trackingId != null) {
