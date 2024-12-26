e8f3cdfa83a58aba86c7f8300139190a1f6e1361
kathrin.ulmer <64363092+bluebell185@users.noreply.github.com>
2024-12-26 18:08:43 +0100

    Funktionalität FeatureOne-Icon und Navigation zwischen den Widgets

--- master_projekt/lib/start_analysis.dart
+++ master_projekt/lib/start_analysis.dart
@@ -5,6 +5,7 @@ import 'package:master_projekt/face_analysis.dart';
 import 'package:master_projekt/feature_one.dart';
 import 'package:master_projekt/json_parse.dart';
 import 'package:master_projekt/results_check.dart';
+import 'package:master_projekt/screen_with_deepar_camera.dart';
 
 // UI-Elemente
 import 'package:master_projekt/ui/toolbar.dart';
@@ -14,8 +15,11 @@ import 'package:master_projekt/ui/buttons.dart';
 // TO DO:
 // pngs übel unscharf, Flutter an sich kann nicht mit svgs -> flutter plugin zur svg static image verarbeitung
 
+final String startAnalysisWidgetName = 'StartAnalysis';
+
 bool isAnalysisStarted = false;
 late RoisData roiData;
+bool isGoingBackAllowedInNavigator = false;
 
 class StartAnalysis extends StatefulWidget {
   StartAnalysis({super.key, required this.title});
@@ -31,164 +35,195 @@ class _StartAnalysisState extends State<StartAnalysis> {
 
   @override
   Widget build(BuildContext context) {
-    return CameraWidget(
-      title: 'Kamerabild 1',
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Stack(
-          children: [
-            // Main content background container
-            Container(
-              width: double.infinity,
-              height: double.infinity,
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 70),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  // Title oben
-                  ScreenTitle(
-                    titleText: 'Analysis',
-                    titleColor: Colors.white,
+    return PopScope(
+        canPop: isGoingBackAllowedInNavigator,
+        child: CameraWidget(
+          title: 'Kamerabild 1',
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Stack(
+              children: [
+                // Main content background container
+                Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 70),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      // Title oben
+                      ScreenTitle(
+                        titleText: 'Analysis',
+                        titleColor: Colors.white,
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-
-            Toolbar(),
-
-            if (showRecommendations)
-              // CTA Button unten
-              Positioned(
-                left: 0,
-                right: 0,
-                bottom: 70,
-                child: Center(
-                  child: isLoading
-                      ? CircularProgressIndicator(
-                          valueColor:
-                              AlwaysStoppedAnimation<Color>(Colors.white),
-                        )
-                      : PrimaryButton(
-                          buttonText: 'start analysis',
-                          onPressed: () async {
-                            setState(() {
-                              isLoading = true;
-                            });
-
-                            // 3 Sekunden warten TODO hier dann den Ladescreen einbauen
-                            await Future.delayed(Duration(seconds: 3));
-
-                            try {
-                              // Gesichtsanalyse starten
-                              await FaceAnalysis.analyseColorsInFace(
-                                  faceForAnalysis);
-                              roiData = await loadRoisData();
-                            } catch (e) {
-                              print('Error during analysis: $e');
-                            } finally {
-                              setState(() {
-                                isLoading = false;
-                                showRecommendations = false;
-                              });
-                            }
-                          },
-                        ),
                 ),
-              ),
-            if (!showRecommendations)
-              ResultsCheckPopUp(
-                popUpHeading: 'Your analysis results',
-                analysisElements: [
-                  // TO DO: ROI-Analyse-Ergebnis aus den Tabs rausnehmen? Drinlassen, aber als default selected?
-                  AnalysisElement(
-                    number: 1,
-                    title: 'Eye color',
-                    result:
-                        eyeColorCategory!.name, // Results der Gesichtsanalyse
-                    options: [
-                      'blue',
-                      'green',
-                      'brown',
-                      'grey'
-                    ], // Optionen der Kategorie
-                    selectedOption: eyeColorCategory!.name,
-                    onOptionSelected: (selected) {
-                      print('Selected for Eye color: $selected');
-                      setEyeColorCategory(selected);
-                    },
-                  ),
-                  AnalysisElement(
-                    number: 2,
-                    title: 'Eye shape',
-                    result:
-                        eyeShapeCategory!.name, // Results der Gesichtsanalyse
-                    options: [
-                      'almond',
-                      'round',
-                      'upturned',
-                      'downturned' //,
-                      //'monolid'
-                    ], // Optionen der Kategorie
-                    selectedOption: eyeShapeCategory!.name,
-                    onOptionSelected: (selected) {
-                      print('Selected for Eye shape: $selected');
-                      setEyeShapeCategory(selected);
-                    },
-                  ),
-                  AnalysisElement(
-                    number: 3,
-                    title: 'Teint color',
-                    result: blushCategory!.name, // Results der Gesichtsanalyse
-                    options: [
-                      'light',
-                      'beige',
-                      'olive',
-                      'tanned',
-                      'dark'
-                    ], // Optionen der Kategorie
-                    selectedOption: blushCategory!.name,
-                    onOptionSelected: (selected) {
-                      print('Selected for Head shape: $selected');
-                      setBlushCategory(selected);
-                    },
-                  ),
-                  AnalysisElement(
-                    number: 4,
-                    title: 'Head shape',
-                    result:
-                        blushShapeCategory!.name, // Results der Gesichtsanalyse
-                    options: [
-                      'square',
-                      'round',
-                      'oval'
-                    ], // Optionen der Kategorie
-                    selectedOption: blushShapeCategory!.name,
-                    onOptionSelected: (selected) {
-                      print('Selected for Head shape: $selected');
-                      setBlushShapeCategory(selected);
-                    },
-                  ),
-                ],
-                onSave: () async {
-                  if (cameraController.value.isInitialized) {
-                    await cameraController.dispose();
-                  }
-                  setState(() {
-                    showRecommendations = true;
-                    isCameraDisposed = true;
-                  });
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => FeatureOne(),
+
+                Toolbar(
+                  widgetCallingToolbar: startAnalysisWidgetName,
+                ),
+
+                if (showRecommendations)
+                  // CTA Button unten
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    bottom: eyeColorCategory == null ? 70 : 140,
+                    child: Center(
+                      child: isLoading
+                          ? CircularProgressIndicator(
+                              valueColor:
+                                  AlwaysStoppedAnimation<Color>(Colors.white),
+                            )
+                          : PrimaryButton(
+                              buttonText: eyeColorCategory == null
+                                  ? 'start analysis'
+                                  : 'repeat analysis',
+                              onPressed: () async {
+                                setState(() {
+                                  isLoading = true;
+                                });
+
+                                // 3 Sekunden warten TODO hier dann den Ladescreen einbauen
+                                await Future.delayed(Duration(seconds: 3));
+
+                                try {
+                                  // Gesichtsanalyse starten
+                                  await FaceAnalysis.analyseColorsInFace(
+                                      faceForAnalysis);
+                                  roiData = await loadRoisData();
+                                } catch (e) {
+                                  print('Error during analysis: $e');
+                                } finally {
+                                  setState(() {
+                                    isLoading = false;
+                                    showRecommendations = false;
+                                  });
+                                }
+                              },
+                            ),
                     ),
-                  );
-                },
-              ),
-          ],
+                  ),
+                if (showRecommendations && eyeColorCategory != null)
+                  Positioned(
+                      left: 0,
+                      right: 0,
+                      bottom: 70,
+                      child: Center(
+                          child: PrimaryButton(
+                        buttonText: 'skip analysis',
+                        onPressed: () async {
+                          await stepsToGoToFeatureOne(context);
+                        },
+                      ))),
+                if (!showRecommendations)
+                  ResultsCheckPopUp(
+                    popUpHeading: 'Your analysis results',
+                    analysisElements: [
+                      // TO DO: ROI-Analyse-Ergebnis aus den Tabs rausnehmen? Drinlassen, aber als default selected?
+                      AnalysisElement(
+                        number: 1,
+                        title: 'Eye color',
+                        result: eyeColorCategory!
+                            .name, // Results der Gesichtsanalyse
+                        options: [
+                          'blue',
+                          'green',
+                          'brown',
+                          'grey'
+                        ], // Optionen der Kategorie
+                        selectedOption: eyeColorCategory!.name,
+                        onOptionSelected: (selected) {
+                          print('Selected for Eye color: $selected');
+                          setEyeColorCategory(selected);
+                        },
+                      ),
+                      AnalysisElement(
+                        number: 2,
+                        title: 'Eye shape',
+                        result: eyeShapeCategory!
+                            .name, // Results der Gesichtsanalyse
+                        options: [
+                          'almond',
+                          'round',
+                          'upturned',
+                          'downturned' //,
+                          //'monolid'
+                        ], // Optionen der Kategorie
+                        selectedOption: eyeShapeCategory!.name,
+                        onOptionSelected: (selected) {
+                          print('Selected for Eye shape: $selected');
+                          setEyeShapeCategory(selected);
+                        },
+                      ),
+                      AnalysisElement(
+                        number: 3,
+                        title: 'Teint color',
+                        result:
+                            blushCategory!.name, // Results der Gesichtsanalyse
+                        options: [
+                          'light',
+                          'beige',
+                          'olive',
+                          'tanned',
+                          'dark'
+                        ], // Optionen der Kategorie
+                        selectedOption: blushCategory!.name,
+                        onOptionSelected: (selected) {
+                          print('Selected for Head shape: $selected');
+                          setBlushCategory(selected);
+                        },
+                      ),
+                      AnalysisElement(
+                        number: 4,
+                        title: 'Head shape',
+                        result: blushShapeCategory!
+                            .name, // Results der Gesichtsanalyse
+                        options: [
+                          'square',
+                          'round',
+                          'oval'
+                        ], // Optionen der Kategorie
+                        selectedOption: blushShapeCategory!.name,
+                        onOptionSelected: (selected) {
+                          print('Selected for Head shape: $selected');
+                          setBlushShapeCategory(selected);
+                        },
+                      ),
+                    ],
+                    onSave: () async {
+                      await stepsToGoToFeatureOne(context);
+                    },
+                  ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Future<void> stepsToGoToFeatureOne(BuildContext context) async {
+    if (cameraController.value.isInitialized) {
+      await cameraController.dispose();
+    }
+    setState(() {
+      showRecommendations = true;
+      isCameraDisposed = true;
+      shouldCalcRoiButtons = true;
+    });
+    if (isGoingBackAllowedInNavigator) {
+      isGoingBackAllowedInNavigator = false;
+      Navigator.pop(context);
+    } else {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+          builder: (context) => FeatureOne(),
         ),
-      ),
-    );
+        (route) => false, // Entfernt alle vorherigen Routen
+      );
+    }
   }
 }
